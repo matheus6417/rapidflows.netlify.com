a3ca3507c611a4f96748eff6e2edbdf302d342a8
Matheus Cezar <matheus6417@gmail.com>
2019-05-26 11:14:49 -0300

    fix bug where navbar background where strange

---
+++ js/main.js
@@ -0,0 +1,27 @@
+﻿(function () {
+
+    var navbar = {};
+    
+    navbar.init = function () {
+        $(window).scroll(function () {
+            var scroll = $(window).scrollTop();
+
+            if (scroll >= 150) {
+                $(".navbar").addClass("is-sticky");
+
+            } else {
+                $(".navbar").removeClass("is-sticky");
+
+            }
+        });
+    };
+
+
+
+    $(document).ready(function () {
+        navbar.init();
+     
+
+    });
+
+})();
