3af042790a0974f2338e5647a88066475ad0d178
Matheus Cezar <matheus6417@gmail.com>
2019-06-13 21:11:04 -0300

    fix some itens

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 ﻿(function () {
 
     var navbar = {};
-    
+    var features = {};
+
     navbar.init = function () {
         $(window).scroll(function () {
             var scroll = $(window).scrollTop();
@@ -16,10 +17,25 @@
         });
     };
 
+    features.init = function () {
+
+        // Active the first thumb & panel
+        $(".tabs-thumb:first-child").addClass("is-active").closest(".tabs").find(".tabs-panel:first-child")
+            .show();
+
+        $(".tabs-thumb").click(function () {
+            // Cancel the siblings
+            $(this).siblings(".tabs-thumb").removeClass("is-active").closest(".tabs").find(".tabs-panel").hide();
+            // Active the thumb & panel
+            $(this).addClass("is-active").closest(".tabs").find(".tabs-panel").eq($(this).index(
+                ".tabs-thumb")).show();
+        });
+    }
 
 
     $(document).ready(function () {
         navbar.init();
+        features.init();
         (function () {
             document.getElementsByTagName('body')[0].classList.remove("fade-out");
         }());
