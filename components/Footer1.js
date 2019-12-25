e75ff2694dc98d81ec46fdf6779c51c4dbd3c24b
Matheus Cezar <matheus6417@gmail.com>
2019-12-25 19:53:50 -0300

    fixes

---
+++ components/Footer1.js
@@ -0,0 +1,84 @@
+
+const Footer1 = {
+    template: `
+      <div>
+          <div class="section footer">
+          <div class="main-container w-container">
+            <div class="footer-row w-clearfix">
+              <div class="footer-column">
+                <div class="footer-title">Rapid4 Features</div><a
+                  href="#"
+                  class="footer-link"
+                >Webforms</a><a
+                  href="#"
+                  class="footer-link"
+                >Workflows</a><a
+                  href="#"
+                  class="footer-link"
+                >Views</a><a
+                  href="#"
+                  class="footer-link"
+                >Reports</a><a
+                  href="#"
+                  class="footer-link"
+                >RapidMobile</a>
+              </div>
+              <div class="footer-column">
+                <div class="footer-title">From the RapidStore</div><a
+                  href="#"
+                  target="_blank"
+                  class="footer-link"
+                >Featured Apps</a>
+              </div>
+              <div class="footer-column">
+              <div class="footer-title">Partner with Blynk</div><a
+                  href="#"
+                  target="_blank"
+                  class="footer-link"
+                >Blynk Alliance Program Apps</a>
+                <a
+                  href="#"
+                  target="_blank"
+                  class="footer-link"
+                >BlynkAMP</a>
+                <a
+                  href="#"
+                  target="_blank"
+                  class="footer-link"
+                >Technical Resources</a>
+              </div>
+              <div class="footer-column">
+              <div class="footer-title">About Blynk</div><a
+                  href="#"
+                  target="_blank"
+                  class="footer-link"
+                >The Blynk Team</a>
+                <a
+                  href="#"
+                  target="_blank"
+                  class="footer-link"
+                >Contact Us</a>
+                <a
+                  href="#"
+                  target="_blank"
+                  class="footer-link"
+                >Twitter Feed</a>
+                <a
+                  href="#"
+                  target="_blank"
+                  class="footer-link"
+                >Blynk Blog</a>
+              </div>
+            </div>
+          
+            <div class="bottom-footer-block">
+              <div class="footer-text">Copyright (C) 2015 Blynk Pte Ltd. All Rights Reserved</div><br><a
+                href="/privacy"
+                class="footer-small-link"
+              >Blynk Offices Worldwide</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    `
+  };
